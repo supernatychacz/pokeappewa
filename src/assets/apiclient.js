360c1fd775ebe4ab47b6ca08da5c39b6d821763e
supernatychacz <supernatychacz@gmail.com>
2023-03-19 13:53:15 +0000

    push

--- src/assets/apiclient.js
+++ src/assets/apiclient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://pokemonfastapi-sj1x.onrender.com",
+  baseURL: "https://pikapika.onrender.com/",
   headers: {
     "Content-Type": "application/json",
     // "Authorisation":
